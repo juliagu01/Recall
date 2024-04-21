7c738fdc1756cb93033ee5ad87ab1d6f71ff4f5b
Julia Gu <julia.gu01@gmail.com>
2024-04-20 23:12:19 -0700

    Begin habits page

--- Recall/pages/habits.py
+++ Recall/pages/habits.py
@@ -4,7 +4,30 @@ from Recall.templates import template
 
 import reflex as rx
 
+habits_data: list[dict[str, str]] = [
+    {
+        "name": "Target run",
+        "labels": "Items?|Spending?",
+    },
+    {
+        "name": "Sleep quality",
+        "labels": "Rating (1-5):",
+    },
+    {
+        "name": "Vacuum",
+        "labels": "",
+    },
+]
 
+
+class ToggleEditViewHabits(rx.State):
+    view: str = "Edit"
+
+    def toggle(self):
+        if (self.view == "Edit"):
+            self.view = "Save"
+        else:
+            self.view = "Edit"
 
 
 @template(route="/habits", title="Habits")
@@ -14,12 +37,52 @@ def habits() -> rx.Component:
     Returns:
         The UI for the Habits page.
     """
-    return rx.vstack(
-        rx.heading("Habits", size="8"),
-        rx.text(
-            "You can edit this page in ",
-            rx.code("{your_app}/pages/account.py"),
-        ), 
+    return rx.hstack(
+        rx.vstack(
+            rx.hstack(
+                rx.heading(
+                    "Habits", 
+                    size="6", 
+                    font_family="Lexend",
+                ),
+                rx.button(
+                    ToggleEditViewHabits.view,
+                    on_click=ToggleEditViewHabits.toggle,
+                ),
+                width="100%",
+                justify="between",
+            ),
+            rx.foreach(
+                habits_data,
+                lambda habit: rx.vstack(
+                    rx.heading(
+                        habit["name"],
+                        size="5", 
+                        font_family="Lexend",
+                    ),
+                    rx.text(
+                        habit["labels"],
+                    ),
+#                    rx.heading(
+#                        habit["name"],
+#                        size="5", 
+#                        font_family="Lexend",
+#                    ),
+#                    rx.foreach(
+#                        habit["labels"].split("|").append("Notes:"),
+#                        lambda label: rx.text(
+#                            label
+#                        ),
+#                    ),
+                ),
+            ),
+            rx.text(
+                "You can edit this page in ",
+                rx.code("{your_app}/pages/account.py"),
+            ), 
+            width="40vw",
+            align="center",
+        ),
         width="100vw",
-        align="center",
+        justify="center",
     )
